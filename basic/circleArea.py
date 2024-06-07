ee3c5efa3945f40fae2f2851cd37998169ef2aba
Setas8 <dcsetas@gmail.com>
2024-06-07 11:15:14 +0200

    add basics

---
+++ basic/circleArea.py
@@ -0,0 +1,6 @@
+from math import pi, pow
+
+r = float(input("Radio: "))
+area = pi * pow(r,2)
+
+print("Radio: %.2f --> Circle area:%.2f" % (r,area))
---
+++ basic/listTupleNumbers.py
@@ -0,0 +1,7 @@
+numbers = input("Numbers separate with ',': ")
+
+listNumbers = numbers.split(",")
+tupleNumbers = tuple(listNumbers)
+
+print("List: ", listNumbers)
+print("Tuple: ", tupleNumbers)
